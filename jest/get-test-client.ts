54b02a900f2efa49e04979e564a24a8ab8cfd5f7
ejnshtein <ejnshtein@gmail.com>
2021-01-09 18:30:42 +0200

    fix jest test client

--- jest/get-test-client.ts
+++ jest/get-test-client.ts
@@ -4,16 +4,16 @@ import path from 'path'
 
 require('dotenv').config('../.env')
 
-const sessionPath = path.join(__dirname, '..', 'session')
+// const sessionPath = path.join(__dirname, '..', 'session')
 
-const pathExists = async (path: string): Promise<boolean> => {
-  try {
-    await fs.promises.access(path, fs.constants.F_OK)
-    return true
-  } catch {
-    return false
-  }
-}
+// const pathExists = async (path: string): Promise<boolean> => {
+//   try {
+//     await fs.promises.access(path, fs.constants.F_OK)
+//     return true
+//   } catch {
+//     return false
+//   }
+// }
 
 export const getTestClient = async (): Promise<Nyaa> => {
   return new Nyaa()
@@ -22,14 +22,14 @@ export const getTestClient = async (): Promise<Nyaa> => {
 export const getAuthenticatedTestClient = async (): Promise<Nyaa> => {
   const client = new Nyaa()
 
-  if (await pathExists(sessionPath)) {
-    await client.agent.loginWithSession(sessionPath)
-  } else {
-    await client.agent.login(
-      process.env.NYAA_USERNAME,
-      process.env.NYAA_PASSWORD
-    )
-    await client.agent.saveSession(sessionPath)
-  }
+  // if (await pathExists(sessionPath)) {
+  //   await client.agent.loginWithSession(sessionPath)
+  // } else {
+  //   await client.agent.login(
+  //     process.env.NYAA_USERNAME,
+  //     process.env.NYAA_PASSWORD
+  //   )
+  //   await client.agent.saveSession(sessionPath)
+  // }
   return client
 }
