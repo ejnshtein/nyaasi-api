b3c9ec09fae0d11fd276d9991af94aca7d5d3bf1
Okabe Rintaro <ejnshtein@gmail.com>
2021-10-30 01:09:18 +0300

    remove unused test, cleanup tests

--- __tests__/nyaa.test.ts
+++ __tests__/nyaa.test.ts
@@ -1,41 +1,27 @@
 import fs from 'fs'
 import path from 'path'
 import { getTestClient } from '../jest/get-test-client'
-import { parseSearch, parseTorrent } from '../src/Scraper'
+// import { parseSearch, parseTorrent } from '../src/Scraper'
+
+const saveResultToFile = <T>(result: T, filename: string) =>
+  fs.promises.writeFile(
+    path.join(process.cwd(), '.tmp', filename),
+    JSON.stringify(result, null, 2),
+    'utf-8'
+  )
 
 /**
  * TODO add more tests
  */
 describe('nyaa api', () => {
-  // it('search test', async () => {
-  //   const pagehtml = await fs.promises.readFile(
-  //     path.join(__dirname, '..', 'Browse __ Nyaa.htm'),
-  //     'utf-8'
-  //   )
-
-  //   const result = parseSearch(pagehtml)
-
-  //   expect(true).toEqual(true)
-  // })
-
-  // it('test view parser', async () => {
-  //   const pagehtml = await fs.promises.readFile(
-  //     path.join(__dirname, '..', 'view.htm'),
-  //     'utf-8'
-  //   )
-
-  //   const result = parseTorrent(1324807, pagehtml)
-
-  //   expect(true).toEqual(true)
-  // })
-
-  // return
   it('should search for Kotonoha no Niwa rip by beatrice-raws', async () => {
     const client = await getTestClient()
     const result = await client.search({
       title: 'Kotonoha no Niwa',
       category: '1_2'
     })
+
+    await saveResultToFile(result, 'search.test.json')
 
     expect(
       result.torrents.some(
@@ -46,17 +32,11 @@ describe('nyaa api', () => {
     ).toEqual(true)
   })
 
-  // it('should get Kotonoha no Niwa torrent from api', async () => {
-  //   return // disabled
-  //   const client = await getTestClient()
-  //   const result = await client.getTorrent(890127)
-
-  //   expect(result.submitter.name).toEqual('DJATOM')
-  // })
-
   it('should get Kotonoha no Niwa torrent anonymously', async () => {
     const client = await getTestClient()
     const result = await client.getTorrentAnonymous(890127)
+
+    await saveResultToFile(result, 'torrent.test.json')
 
     expect(result.submitter.name).toEqual('DJATOM')
   })
